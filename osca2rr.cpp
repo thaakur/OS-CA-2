e77367ba8022d2fb3ff3f36e2591ac561c6b1e4c
Avinash Kumar <avirms07@gmail.com>
2020-03-27 05:51:59 +0530

    Update osca2rr.cpp

--- osca2rr.cpp
+++ osca2rr.cpp
@@ -1,4 +1,6 @@
-#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+
 void rr(int no,int remt[10],int Cur_t,int arT[10], int bsT[10]);
 
 int main()
@@ -7,22 +9,22 @@ int main()
 	indicator = 0;
 	wait = 0;
 	tut = 0;
-	cout<<"Enter number of processes ";
-	cin>>no;
+	printf("Enter number of processes ");
+	scanf("%d",&no);
 	RemProc = no;
 
-	cout<<"\nEnter the arrival time and burst time of the processes\n";
+	printf("\nEnter the arrival time and burst time of the processes\n");
 	for(Proc_no = 0;Proc_no < no;Proc_no++)
 	{
-		cout<<"\nProcess P\n",Proc_no+1;
-		cout<<"Arrival time = ";
-		cin>>arT[Proc_no];
-		cout<<"Burst time = ";
-		cin>>bsT[Proc_no];
+		printf("\nProcess P%d\n",Proc_no+1);
+		printf("Arrival time = ");
+		scanf("%d",&arT[Proc_no]);
+		printf("Burst time = ");
+		scanf("%d",&bsT[Proc_no]);
 		remt[Proc_no]=bsT[Proc_no];
 	}
-	cout<<"The details of time quantum are as follows:\n";
-	cout<<"The time quantum for first round is 3.\n";
+	printf("The details of time quantum are as follows:\n");
+	printf("The time quantum for first round is 3.\n");
 	time_quan=3;
 	CurT=0;
 	for(Proc_no=0;RemProc!=0;)
@@ -39,11 +41,11 @@ int main()
 			CurT+=time_quan;
 		}
 		if(remt[Proc_no]==0 && indicator==1)
-		{ cout<<Proc_no;
+		{ printf("%d",Proc_no);
 			RemProc--;
-			cout<<Proc_no+1;
-			cout<<"\t\t\t"<<CurT-arT[Proc_no]<<endl;
-			cout<<"\t\t\t"<<CurT-bsT[Proc_no]-arT[Proc_no]<<endl;
+			printf("P %d",Proc_no+1);
+			printf("\t\t\t%d",CurT-arT[Proc_no]);
+			printf("\t\t\t%d\n",CurT-bsT[Proc_no]-arT[Proc_no]);
 			wait+=CurT-arT[Proc_no]-bsT[Proc_no];
 			tut+=CurT-arT[Proc_no];
 			indicator=0;
@@ -77,4 +79,53 @@ int main()
 void rr(int no,int remt[10],int Cur_t,int arT[10], int bsT[10]){
 
 	float avg_wait,avg_tut;
-	
+	int i,j,n=no,temp,btime[20],Proc_no[20],w_time[20],tut_t[20],total=0,loc;
+
+    printf("Third round with least burst time.\n");
+
+    for(i=0;i<n;i++)
+    {
+        btime[i]=remt[i];
+        w_time[i]=Cur_t-arT[i]-btime[i];
+		Proc_no[i]=i+1;
+    }
+
+    for(i=0;i<n;i++)
+    {
+        loc=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(btime[j]<btime[loc]){
+            	loc=j;
+            }
+        }
+        temp=btime[i];
+        btime[i]=btime[loc];
+        btime[loc]=temp;
+        temp=Proc_no[i];
+        Proc_no[i]=Proc_no[loc];
+        Proc_no[loc]=temp;
+    }
+
+    for(i=1;i<n;i++)
+    {
+        for(j=0;j<i;j++){
+        	w_time[i]+=btime[j];
+        }
+        total+=w_time[i];
+    }
+
+    avg_wait=(float)total/n;
+    total=0;
+    printf("\nProcess\t\tBurst time\t\twaiting time\t\tTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tut_t[i]=btime[i]+w_time[i];
+        total=total + tut_t[i];
+        printf("\nP%d\t\t\t%d\t\t\t%d\t\t\t%d",Proc_no[i],btime[i],w_time[i],tut_t[i]);
+    }
+    avg_tut=(float)total/n;
+    printf("\n\nAverage waiting time = %f",avg_wait);
+    printf("\n Average turnaround time = %f\n",avg_tut);
+
+}
